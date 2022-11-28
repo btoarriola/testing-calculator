1210765bfe39e533bdbb0cf0343dec6681f5605d
btoarriola <betokanon@gmail.com>
2022-11-28 14:43:35 -0600

    new branch exp

--- src/app/ui/ui.component.spec.ts
+++ src/app/ui/ui.component.spec.ts
@@ -397,3 +397,102 @@ describe('Ui Division - Component', () => {
   });
 
 });
+
+//------------------------------------ PRUEBA EXP ------------------------------------
+describe('Ui Division - Component', () => {
+  let component: UiComponent;
+  let fixture: ComponentFixture<UiComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ UiComponent ],
+      imports: [FormsModule],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UiComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('Should call exp method', () => {
+     // Arrange
+     let result = 0;
+     component.operator1 = 2;
+     component.operator2 = 2;
+ 
+     // Act
+     component.exp();
+     result = component.result;
+ 
+     // Assert
+     expect(result).toBe(4);
+  });
+
+
+
+  it('Should set operator1 model through ngModel', async() => {
+    // Arrange 
+    await fixture.whenStable();
+    fixture.detectChanges();
+    const inputElement = fixture.debugElement.query(By.css('input[name="operator1"]')).nativeElement;
+
+    // Act 
+    inputElement.value = '3.1416';
+    inputElement.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    // Assert 
+    expect(component.operator1).toEqual(3.1416);
+  });
+ 
+  it('Should set operator2 model through ngModel', async() => {
+    // Arrange 
+    await fixture.whenStable();
+    fixture.detectChanges();
+    const inputElement = fixture.debugElement.query(By.css('input[name="operator2"]')).nativeElement;
+
+    // Act 
+    inputElement.value = '2.71';
+    inputElement.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    // Assert 
+    expect(component.operator2).toEqual(2.71);
+  });
+
+
+  it('should add operator1 and operator2 when i click the exp button ', () => {
+    // Arrange 
+    component.operator1 = 5.0;
+    component.operator2 = 2.0;
+    let expButton = fixture.debugElement.query(By.css('.exp-button'));
+
+    // Act
+    expButton.triggerEventHandler('click', null);
+
+    // Assert
+    expect(component.result).toBe(25);
+
+   });
+
+  it('Should render exp in result div', () => {
+    // Arrange
+    component.operator1 = 10;
+    component.operator2 = 3;
+ 
+    // Act
+    component.exp();
+    fixture.detectChanges();
+    
+    let de = fixture.debugElement.query(By.css('.result'));
+    let el : HTMLElement = de.nativeElement;
+
+    // Assert
+    expect(el.innerText).toContain('1000');
+     
+  });
+
+});
